b05474952c0ea83ba6a227911aaec11183e099f3
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-11 16:23:11 +0900

    .

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect, url_for, session
 
 # 객체생성
 app = Flask(__name__)
+app.secret_key = 'secret key'
+
 
 @app.route('/')
 def index():
@@ -19,8 +21,17 @@ def projects():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        return render_template('contact.html', success=True)
-    return render_template('contact.html', success=False)
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        print(f'제출된 데이터 이름:{name} 이메일:{email} 메세지:{message}')
+        # 리다이렉트 success 표시
+        session['success'] = True
+        return redirect(url_for('contact'))  # get 방식
+    
+    # get요청시 success 표시 후 초기화
+    success = session.pop('success',False)
+    return render_template('contact.html', success=success)
 
 
 if __name__ == '__main__':
