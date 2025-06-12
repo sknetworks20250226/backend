b6d62a5346a3da0d2f686cc25efc32a1089d43ad
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-12 10:10:04 +0900

    .

---
+++ api/backend/model.py
@@ -0,0 +1,9 @@
+# 데이터 베이스 테이블 정의
+from sqlalchemy import Column,Integer,String
+from database import Base
+class User(Base):
+    __tablename__ ='users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String,unique=True,index=True)
+    email = Column(String, unique=True,index=True)
+    password = Column(String)  # 일단 보안은 나중에.
